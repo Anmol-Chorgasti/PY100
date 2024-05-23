42ddba6c6ae32dec9f687eb8eb7a39838327afb0
Anmol-Chorgasti <anmolchorgasti@gmail.com>
2024-05-23 06:40:22 +0200

    finished exercises of using_collections

--- using_collections.py
+++ using_collections.py
@@ -32,3 +32,59 @@ print(pets.get('Lizard', '<silence>'))
 #q7
 info = 'xyz:*:42:42:Lee Kim:/home/xyz:/bin/zsh'
 print(info.replace(':','+')) #can also use split by : and then join using +
+
+#q9
+stuff = [
+    ['hello', 'world'],
+    ['example', 'mem', None, 6, 88],
+    [4, 8, 12],
+]
+stuff[1][-2] = 606
+print(stuff)
+
+#q10
+cats = {
+    'Pete': {
+        'Cheddar': {
+            'color': 'orange',
+            'enjoys': {
+                'sleeping',
+                'snuggling',
+                'meowing',
+                'eating',
+                'birdwatching',
+            },
+        },
+        'Cocoa': {
+            'color': 'black',
+            'enjoys': {
+                'eating',
+                'sleeping',
+                'playing',
+                'chewing',
+            },
+        },
+    },
+}
+print(cats['Pete']['Cocoa']['enjoys'])
+
+#q12
+numbers1 = [1, 3, 5, 7, 9, 11]
+numbers2 = []
+numbers3 = [2, 4, 6, 8]
+numbers4 = ['1', '3', '5']
+numbers5 = ['1', 3.0, '5']
+print(3 in numbers1)
+print(3 in numbers2)
+print(3 in numbers3)
+print(3 in numbers4)
+print(3 in numbers5)
+
+#q14
+my_str = 'abc'
+my_list = ['Alpha', 'Bravo', 'Charlie']
+my_tuple = (None, True, False)
+my_range = range(10, 60, 10)
+
+combined_tuple = list(zip(my_str, my_list, my_tuple, my_range))
+print(combined_tuple)
